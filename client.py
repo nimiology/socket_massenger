f54332d2419b87ed7d8d1448d6231bc57c3c2aa7
Nima <nimiologyy@gmail.com>
2021-06-07 11:07:43 +0430

    GOH KHORDAM

--- client.py
+++ client.py
@@ -1,73 +1,18 @@
-import socket
+mport socket
 import os
 import threading
-import tkinter
 
 
+WORKING = True
+SERVER = input('What is the server IP?')
+PORT = int(input('What is the server PORT?'))
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER,PORT)
 
-
-USERS = []
-MESSAGES = []
-
-def HOST():
-    HOSTADDR = []
-    DIALOG = tkinter.Tk()
-    DIALOG.geometry('455x70')
-    LABEL = tkinter.Label(DIALOG,text='What is the server IP?')
-    INPUT1 = tkinter.Entry(DIALOG,width=40)
-    BUTTON = tkinter.Button(DIALOG,text='Submit',width=7,height=1)
-    LABEL.place(x=5,y=5)
-    INPUT1.place(x=5,y=25)
-    BUTTON.place(x=380,y=29)
-
-    def BIND(event):
-        TXT = INPUT1.get()
-        HOSTADDR.append(TXT)
-        DIALOG2 = tkinter.Tk()
-        DIALOG2.geometry('455x70')
-        LABEL = tkinter.Label(DIALOG2,text='What is the server PORT?')
-        INPUT = tkinter.Entry(DIALOG2,width=40)
-        BUTTON = tkinter.Button(DIALOG2,text='Submit',width=7,height=1)
-        LABEL.place(x=5,y=5)
-        INPUT.place(x=5,y=25)
-        BUTTON.place(x=380,y=29)
-
-        def PORT(event):
-            TXT = int(INPUT.get())
-            HOSTADDR.append(TXT)
-            DIALOG2.quit()
-            DIALOG2.destroy()
-
-        BUTTON.bind('<ButtonRelease-1>',PORT)
-        DIALOG2.mainloop()
-        DIALOG.quit()
-        DIALOG.destroy()
-    
-    
-    BUTTON.bind('<ButtonRelease-1>',BIND)
-    DIALOG.mainloop()
-
-    return (HOSTADDR[0],HOSTADDR[1])
-
-
-def NAMEgetter():
-    NAME = []
-    DIALOG = tkinter.Tk()
-    DIALOG.geometry('455x70')
-    LABEL = tkinter.Label(DIALOG,text='What is your Name?')
-    INPUT1 = tkinter.Entry(DIALOG,width=40)
-    BUTTON = tkinter.Button(DIALOG,text='Submit',width=7,height=1)
-    LABEL.place(x=5,y=5)
-    INPUT1.place(x=5,y=25)
-    BUTTON.place(x=380,y=29)
-    def BIND(event):
-        TXT = INPUT1.get()
-        NAME.append(TXT)
-
-    BUTTON.bind('<ButtonRelease-1>',BIND)
-    DIALOG.mainloop()
-
-    return NAME[0]
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
+print('[CONNECTION] connected successfully')
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -78,62 +23,17 @@ def send(msg):
     client.send(message)
 
 
+
+
+
 def RECIVER():
     while WORKING:
         DATA = client.recv(2048).decode(FORMAT)
-        if '!!!thisis' in DATA :
-            USERS.append(DATA[9:])
-            TEXT = ''
-            for NAME in USERS:
-                TEXT += f'[{NAME}]\n'
-
-            UPDATERTEXT(TEXTUSERS,NAME)
-        else:
-            #TODO: show message
-            MESSAGES.append(DATA)
-            TEXT = ''
-            for MESSAGE in MESSAGES:
-                TEXT += f'{MESSAGE}'
-
-            UPDATERTEXT(TEXTUSERS,NAME)
         print(DATA)
 
-
-
-def UPDATERTEXT(TEXTTABLE,text):
-    TEXTTABLE.delete(1.0, "end")
-    TEXTTABLE.insert(1.0, text)
-
-
-
-ADDR = HOST()
-WORKING = True
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
-print('[CONNECTION] connected successfully')
-send(f'!!!thisis{NAMEgetter()}')
 print(f"if you want to exit type : {DISCONNECT_MESSAGE}")
 THREAD = threading.Thread(target=RECIVER)
 THREAD.start()
-
-WINDOW = tkinter.Tk()
-WINDOW.geometry('900x600')
-INPUT = tkinter.Entry(WINDOW,width=60)
-TEXTUSERS = tkinter.Text(WINDOW,width=28,height=37,bg='Black',fg='white')
-TEXTUSERS.insert(tkinter.END,"There's No user in server",)
-BUTTON = tkinter.Button(WINDOW,text='SEND!',width=10)
-TEXTMESSAGES = tkinter.Text(WINDOW,width=91,height=34,bg='Black',fg='white')
-TEXTMESSAGES.insert(tkinter.END,"MSG")
-TEXTMESSAGES.place(x=10,y=10)
-BUTTON.place(x=570,y=553)
-TEXTUSERS.place(x=670,y=10)
-INPUT.place(x=10,y=550)
-WINDOW.mainloop()
-
-
-
 while WORKING:
     MSG = input()
     send(MSG)
